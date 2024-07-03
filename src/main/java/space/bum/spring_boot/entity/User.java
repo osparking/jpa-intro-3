74236802f171a32df81de40636254d75fdb50a4b
osparking <jbpark03@gmail.com>
2024-07-03 16:21:41 +0900

    주키를 identity 전략으로 생산한다

---
+++ src/main/java/space/bum/spring_boot/entity/User.java
@@ -0,0 +1,21 @@
+package space.bum.spring_boot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "bumsoap_user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+}
