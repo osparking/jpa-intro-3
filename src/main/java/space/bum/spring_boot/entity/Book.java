7900dd16e8cc0f04320d0fdb9bf8514c27d5a462
osparking <jbpark03@gmail.com>
2024-07-03 10:34:08 +0900

    JPA 에서 EmbeddedId 사용한 합성키 정의

---
+++ src/main/java/space/bum/spring_boot/entity/Book.java
@@ -0,0 +1,18 @@
+package space.bum.spring_boot.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Book {
+  @EmbeddedId
+  private BookId bookId;
+
+}
