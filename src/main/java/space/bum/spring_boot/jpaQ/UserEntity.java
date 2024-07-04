1ede4b3cdef6882d0c0c7a074991bed9ef3dd7e7
osparking <jbpark03@gmail.com>
2024-07-04 12:17:16 +0900

    개체 클래스 자체에 정의된 NamedQuery

--- src/main/java/space/bum/spring_boot/jpaQ/UserEntity.java
+++ src/main/java/space/bum/spring_boot/jpaQ/UserEntity.java
@@ -2,6 +2,7 @@ package space.bum.spring_boot.jpaQ;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +13,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@NamedQuery(name = "UserEntity.findByUserId", 
+            query = "SELECT u FROM UserEntity u WHERE u.id=:userId")
 public class UserEntity {
   @Id
   private Long id;
