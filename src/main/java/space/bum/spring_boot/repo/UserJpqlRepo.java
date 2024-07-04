58835aced88d80300cb2de22b6b10658640491dc
osparking <jbpark03@gmail.com>
2024-07-04 11:42:08 +0900

    JPQL select 문 시험 - TypedQuery 판

--- src/main/java/space/bum/spring_boot/repo/UserJpqlRepo.java
+++ src/main/java/space/bum/spring_boot/repo/UserJpqlRepo.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import lombok.Data;
 import space.bum.spring_boot.jpaQ.UserEntity;
 
@@ -20,5 +21,13 @@ public class UserJpqlRepo {
         .createQuery("SELECT u FROM UserEntity u WHERE u.id=:id");
     jpqlQuery.setParameter("id", id);
     return (UserEntity) jpqlQuery.getSingleResult();
+  }
+
+  public UserEntity getUserByIdWithTypedQuery(Long id) {
+    TypedQuery<UserEntity> typedQuery = getEntityManager().createQuery(
+        "SELECT u FROM UserEntity u WHERE u.id=:id", UserEntity.class);
+
+    typedQuery.setParameter("id", id);
+    return typedQuery.getSingleResult();
   }
 }
