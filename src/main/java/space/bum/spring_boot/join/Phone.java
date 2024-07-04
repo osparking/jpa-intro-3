1282ed19763295f6df7bc30a3a2479a97932e1bd
osparking <jbpark03@gmail.com>
2024-07-05 07:47:47 +0900

    기본이 되는 세 개체 클래스 - 전화

--- src/main/java/space/bum/spring_boot/join/Phone.java
+++ src/main/java/space/bum/spring_boot/join/Phone.java
@@ -1,5 +1,22 @@
 package space.bum.spring_boot.join;
 
-public record Phone() {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Entity
+@Data
+public class Phone {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String number;
+
+  @ManyToOne
+  private Employee employee;
 }
