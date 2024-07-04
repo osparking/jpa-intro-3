f0bef19a9fa974c4170d9b9f1cad046241c6f612
osparking <jbpark03@gmail.com>
2024-07-04 11:51:54 +0900

    TypedQuery 사용 저장소 메소드 시험

--- src/test/java/space/bum/spring_boot/repo/UserJpqlRepoTest.java
+++ src/test/java/space/bum/spring_boot/repo/UserJpqlRepoTest.java
@@ -1,6 +1,6 @@
 package space.bum.spring_boot.repo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +10,28 @@ import space.bum.spring_boot.jpaQ.UserEntity;
 
 @SpringBootTest
 class UserJpqlRepoTest {
-  
+
   @Autowired
   private UserRepository userRepository;
-  
+
   @Autowired
   private UserJpqlRepo userJpqlRepo;
 
+  private static Long id = 1L;
+
   @Test
   void testGetUserByIdWithPlainQuery() {
-    var id = 1L;
     userRepository.save(new UserEntity(id, "김"));
+
+    var userRead = userJpqlRepo.getUserByIdWithPlainQuery(id);
+    assertEquals(id, userRead.getId());
+  }
+
+  @Test
+  void testGetUserByIdWithTypedQuery() {
+    userRepository.save(new UserEntity(id, "이"));
     
-    var userRead =userJpqlRepo.getUserByIdWithPlainQuery(id);
+    var userRead = userJpqlRepo.getUserByIdWithTypedQuery(id);
     assertEquals(id, userRead.getId());
   }
 
