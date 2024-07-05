9be9d3f8a2a2f47e8ba30ad21923b6bea22166f6
osparking <jbpark03@gmail.com>
2024-07-05 13:48:54 +0900

    사람 개체 추가 거래적 메소드

--- src/main/java/space/bum/spring_boot/insert/PersonInsertRepo.java
+++ src/main/java/space/bum/spring_boot/insert/PersonInsertRepo.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 @Repository
 public class PersonInsertRepo {
 
   @PersistenceContext
   private EntityManager entityManager;
+
+  @Transactional
+  public void insertWithQuery(Person person) {
+    entityManager.createNativeQuery("INSERT INTO person "
+        + "(id, first_name, last_name) VALUES (?,?,?)")
+        .setParameter(1, person.getId())
+        .setParameter(2, person.getFirstName())
+        .setParameter(3, person.getLastName())
+        .executeUpdate();
+  }
 }
