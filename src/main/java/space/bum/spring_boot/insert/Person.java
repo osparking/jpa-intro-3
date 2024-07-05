63f2525755c0e6316bb2513875a8495ae993b08b
osparking <jbpark03@gmail.com>
2024-07-05 12:50:09 +0900

    기본 Person 개체

---
+++ src/main/java/space/bum/spring_boot/insert/Person.java
@@ -0,0 +1,19 @@
+package space.bum.spring_boot.insert;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
