6d0f4e686ae7cee8354e16fd2f7be2b528a0ac18
osparking <jbpark03@gmail.com>
2024-07-07 18:51:32 +0900

    개체 상태 전이 시범 - 사람 엔티티

---
+++ src/main/java/space/bum/spring_boot/persist/Person.java
@@ -0,0 +1,15 @@
+package space.bum.spring_boot.persist;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+}
