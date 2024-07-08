9e78d3d2917ee1d60e4aa84f8213a316e8228736
osparking <jbpark03@gmail.com>
2024-07-08 15:11:00 +0900

    년도별 댓글 개수 질의문

--- src/main/java/space/bum/spring_boot/repo/CommentRepo.java
+++ src/main/java/space/bum/spring_boot/repo/CommentRepo.java
@@ -1,11 +1,16 @@
 package space.bum.spring_boot.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import space.bum.spring_boot.aggre.Comment;
 
 @Repository
-public interface CommentRepo extends JpaRepository<Comment, Integer>{
-
+public interface CommentRepo extends JpaRepository<Comment, Integer> {
+  @Query("SELECT c.year, COUNT(c.year) FROM Comment AS c "
+      + "GROUP BY c.year ORDER BY c.year DESC")
+  List<Object[]> countTotalCommentsByYear();
 }
