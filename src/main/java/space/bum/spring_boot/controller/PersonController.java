2f242f58109116efedc790677a5b2f38a8f6ccaa
osparking <jbpark03@gmail.com>
2024-07-08 10:34:17 +0900

    merge 메소드 기능 시험용 제어기 메소드

--- src/main/java/space/bum/spring_boot/controller/PersonController.java
+++ src/main/java/space/bum/spring_boot/controller/PersonController.java
@@ -2,6 +2,7 @@ package space.bum.spring_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
@@ -9,12 +10,13 @@ import space.bum.spring_boot.persist.Person;
 import space.bum.spring_boot.repo.PersonRepo;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
   
   @Autowired
   private PersonRepo personRepo;
 
-  @GetMapping("/person/create")
+  @GetMapping("/create")
   public Long createPerson() {
     Person person = new Person();
     
@@ -22,5 +24,16 @@ public class PersonController {
     personRepo.createPerson(person);
     
     return person.getId();
+  }
+  
+  @GetMapping("/merge")
+  public Person mergePerson() {
+    Person person = new Person(); 
+    
+    person.setName("김가놈");
+    
+    Person merged = (Person) personRepo.mergePerson(person);
+    
+    return merged;
   }
 }
