93ff6dd4d2d8c6eda4122e81206e9ca3c3a26405
osparking <jbpark03@gmail.com>
2024-07-09 08:53:25 +0900

    두 색상 중 하나인 항목 검색 술부

--- src/main/java/space/bum/spring_boot/repo/ItemRepo.java
+++ src/main/java/space/bum/spring_boot/repo/ItemRepo.java
@@ -5,20 +5,25 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Predicate;
 import space.bum.spring_boot.criteria.Item;
 
 public interface ItemRepo extends JpaRepository<Item, Long> {
 
   public default List<Item> findItemByColorAndGrade(
       EntityManager entityManager) {
-    
+
     var criteriaBuilder = entityManager.getCriteriaBuilder();
     var criteriaQuery = criteriaBuilder.createQuery(Item.class);
     var itemRoot = criteriaQuery.from(Item.class);
-    
+
+    Predicate predicateForBlueColor = criteriaBuilder
+        .equal(itemRoot.get("color"), "blue");
+    Predicate predicateForRedColor = criteriaBuilder
+        .equal(itemRoot.get("color"), "red");
+    Predicate predicateForColor = criteriaBuilder.or(predicateForBlueColor,
+        predicateForRedColor);
+
     return null;
   }
 }
