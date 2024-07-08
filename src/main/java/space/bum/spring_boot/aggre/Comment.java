a07e337a65bfb191bd8912643008c2ac4fa3abe6
osparking <jbpark03@gmail.com>
2024-07-08 14:59:05 +0900

    게시판 글/문서에 매달리는 댓글

---
+++ src/main/java/space/bum/spring_boot/aggre/Comment.java
@@ -0,0 +1,22 @@
+package space.bum.spring_boot.aggre;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Comment {
+  @Id
+  private Integer id;
+  private Integer year;
+  private boolean approved;
+  private String content;
+  @ManyToOne
+  private Post post;
+}
