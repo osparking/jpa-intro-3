c6f295a88bf29b97d6bb235d8e771597425cd478
osparking <jbpark03@gmail.com>
2024-07-08 10:33:53 +0900

    merge 메소드 기능 시험용 저장소 메소드

--- src/main/java/space/bum/spring_boot/repo/PersonRepo.java
+++ src/main/java/space/bum/spring_boot/repo/PersonRepo.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import space.bum.spring_boot.persist.Person;
 
+@Slf4j
 @Repository
 public class PersonRepo {
   @Autowired
@@ -14,7 +16,19 @@ public class PersonRepo {
 
   @Transactional
   public Person createPerson(Person person) {
-    session.persist(person);   
+    session.persist(person);
+    return person;
+  }
+
+  @Transactional
+  public Person mergePerson(Person person) {
+    session.persist(person);
+    session.flush();
+    log.info("persist 후 : {}", person.getId());
+    session.evict(person);
+    person.setName("김선생님");
+    person = session.merge(person);
+    log.info("merge 후 : {}", person.getId());
     return person;
   }
 }
