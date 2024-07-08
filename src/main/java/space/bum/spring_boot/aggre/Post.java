e924627609e1fa5863453669639cb28503485872
osparking <jbpark03@gmail.com>
2024-07-08 14:58:28 +0900

    게시판에 올라가는 하나의 글/문서

---
+++ src/main/java/space/bum/spring_boot/aggre/Post.java
@@ -0,0 +1,19 @@
+package space.bum.spring_boot.aggre;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Post {
+    @Id
+    private Integer id;
+    private String title;
+    private String content;
+    @OneToMany(mappedBy = "post")
+    private List comments;
+}
