2f9a7118167c5822914e06aca45c2e40a355dccc
osparking <jbpark03@gmail.com>
2024-07-09 19:57:24 +0900

    책 저장소 빈을 사용하는 제어기 메소드

---
+++ src/main/java/space/bum/spring_boot/controller/BookController.java
@@ -0,0 +1,38 @@
+package space.bum.spring_boot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import static space.bum.spring_boot.repo.impl.Predicate.hasAuthor;
+import static space.bum.spring_boot.repo.impl.Predicate.titleContains;
+
+import space.bum.spring_boot.criteria.Book;
+import space.bum.spring_boot.repo.BookRepository;
+
+@RestController
+public class BookController {
+
+  @Autowired
+  private BookRepository bookRepository;  
+  /**
+   * 서적 저장 메소드
+   */
+  public Book saveBook(Long id, String author, String title) {
+    var book = new Book();
+    book.setId(id);
+    book.setAuthor(author);
+    book.setTitle(title);
+    return bookRepository.save(book);
+  }
+  
+  /**
+   * 서적 검색 메소드 제 2판
+   */
+  public List<Book> findBooksByAuthor(String author) {
+    var list = bookRepository.findAll(hasAuthor(author));
+    return list;
+  }
+
+}
