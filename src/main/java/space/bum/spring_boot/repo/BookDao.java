06f387a346f30590d02c72618b97102ae9a627c5
osparking <jbpark03@gmail.com>
2024-07-09 13:17:15 +0900

    서적 Dao - 저자 및 제목으로 서적 검색 메소드

---
+++ src/main/java/space/bum/spring_boot/repo/BookDao.java
@@ -0,0 +1,33 @@
+package space.bum.spring_boot.repo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.NoArgsConstructor;
+import space.bum.spring_boot.criteria.Book;
+
+@Repository
+@NoArgsConstructor
+public class BookDao {
+  EntityManager em;
+
+  List<Book> findBooksByAuthorNameAndTitle(String authorName, String title) {
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+
+    Root<Book> book = cq.from(Book.class);
+    Predicate authorNamePredicate = cb.equal(book.get("author"), authorName);
+    Predicate titlePredicate = cb.like(book.get("title"), "%" + title + "%");
+    cq.where(authorNamePredicate, titlePredicate);
+
+    TypedQuery<Book> query = em.createQuery(cq);
+    return query.getResultList();
+  }
+}
