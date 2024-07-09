cebac631afa251d23bf7b17725b79f6f0dec4a02
osparking <jbpark03@gmail.com>
2024-07-09 16:39:01 +0900

    책 검색 - 조건 적용 필드 취사선택 가능

--- src/main/java/space/bum/spring_boot/repo/BookRepositoryImpl.java
+++ src/main/java/space/bum/spring_boot/repo/BookRepositoryImpl.java
@@ -1,12 +1,12 @@
 package space.bum.spring_boot.repo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
@@ -16,20 +16,27 @@ import space.bum.spring_boot.criteria.Book;
 
 @Repository
 @NoArgsConstructor
-public class BookRepositoryImpl implements BookCustomRepo{
+public class BookRepositoryImpl implements BookCustomRepo {
   @Autowired
   EntityManager em;
 
-  public List<Book> findBooksByAuthorNameAndTitle(String authorName, String title) {
+  @Override
+  public List<Book> findBooksByAuthorNameAndTitle(String authorName,
+      String title) {
     CriteriaBuilder cb = em.getCriteriaBuilder();
     CriteriaQuery<Book> cq = cb.createQuery(Book.class);
 
     Root<Book> book = cq.from(Book.class);
-    Predicate authorNamePredicate = cb.equal(book.get("author"), authorName);
-    Predicate titlePredicate = cb.like(book.get("title"), "%" + title + "%");
-    cq.where(authorNamePredicate, titlePredicate);
+    List<Predicate> predicates = new ArrayList<>();
 
-    TypedQuery<Book> query = em.createQuery(cq);
-    return query.getResultList();
+    if (authorName != null) {
+      predicates.add(cb.equal(book.get("author"), authorName));
+    }
+    if (title != null) {
+      predicates.add(cb.like(book.get("title"), "%" + title + "%"));
+    }
+    cq.where(predicates.toArray(new Predicate[0]));
+
+    return em.createQuery(cq).getResultList();
   }
 }
