c22a050301d3e063755b974cda4ee4f7a22b9549
osparking <jbpark03@gmail.com>
2024-07-09 11:05:01 +0900

    두 생성자를 item 에 추가해야 정상 작동함

--- src/main/java/space/bum/spring_boot/criteria/Item.java
+++ src/main/java/space/bum/spring_boot/criteria/Item.java
@@ -2,10 +2,14 @@ package space.bum.spring_boot.criteria;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
     @Id
