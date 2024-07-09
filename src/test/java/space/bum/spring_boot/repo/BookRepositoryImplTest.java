f227245412a16a7d419b3818a0bda698a9048128
osparking <jbpark03@gmail.com>
2024-07-09 20:03:08 +0900

    Spec 을 사용하여 저장소 메소드를 호출하는 제어기 메소드 시험

--- src/test/java/space/bum/spring_boot/repo/BookRepositoryImplTest.java
+++ src/test/java/space/bum/spring_boot/repo/BookRepositoryImplTest.java
@@ -7,48 +7,41 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import space.bum.spring_boot.criteria.Book;
+import space.bum.spring_boot.controller.BookController;
 
 @SpringBootTest
 class BookRepositoryImplTest {
 
   @Autowired
-  private BookRepository bookRepository;
+  private BookController bookController;
   
   @BeforeEach
   /**
    * 몇 가지 서적을 다양한 저자와 제목을 할당하여 생성한다.
+   * 
    * @throws Exception
    */
   void setUp() throws Exception {
-    var book = new Book();
-    book.setId(1L);
-    book.setAuthor("조정래");
-    book.setTitle("태백산맥");
-    bookRepository.save(book);
-    
-    book = new Book();
-    book.setId(2L);
-    book.setAuthor("조정래");
-    book.setTitle("아리랑");
-    bookRepository.save(book);
+    bookController.saveBook(1L, "조정래", "태백산맥");
+    bookController.saveBook(2L, "조정래", "아리랑");
   }
 
   @Test
   /**
    * 조정래의 태백산맥을 검색하여 1건이 찾아졌으면 성공이다.
+   * 
    */
   void 만약_조정래의_태백산맥을_검색하여_1건이_찾아졌으면_성공() {
-    var list = bookRepository.findBooksByAuthorNameAndTitle("조정래", "산맥");
+    var list = bookController.findBooksByAuthorTitle("조정래", "아리랑");
     assertEquals(1, list.size());
   }
-  
+
   @Test
   /**
    * 조정래의 모든 저서를 검색하여 2건이 찾아졌으면 성공이다.
    */
   void 만약_조정래의_모든_저서_검색하여_2건이_찾아졌으면_성공() {
-    var list = bookRepository.findBooksByAuthorNameAndTitle("조정래", null);
+    var list = bookController.findBooksByAuthor("조정래");
     assertEquals(2, list.size());
   }
 
