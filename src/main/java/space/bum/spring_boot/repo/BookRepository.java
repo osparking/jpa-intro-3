d1d18652d7156dd62309166bfec59c1889d3a8c0
osparking <jbpark03@gmail.com>
2024-07-09 19:52:59 +0900

    Specification 호출을 위해 확장필요한 인터'

--- src/main/java/space/bum/spring_boot/repo/BookRepository.java
+++ src/main/java/space/bum/spring_boot/repo/BookRepository.java
@@ -1,9 +1,10 @@
 package space.bum.spring_boot.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import space.bum.spring_boot.criteria.Book;
 
 public interface BookRepository
-    extends JpaRepository<Book, Long>, BookCustomRepo {
+    extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 }
